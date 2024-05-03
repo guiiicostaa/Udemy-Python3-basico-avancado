eed903fcecd3f1dc3886081bc74b390b489e75f5
guiiicostaa <164122445+guiiicostaa@users.noreply.github.com>
2024-05-03 18:13:06 -0300

    fix: revisão

--- Exercicios/Condicional-formatacao-fatiamento.py
+++ Exercicios/Condicional-formatacao-fatiamento.py
@@ -23,7 +23,7 @@ if (nome and idade):
   if ('' in nome):
      print('Seu nome contém espaço.')
   else:
-     print('Seu nome não contem espaço.')
+     print('Seu nome não contém espaço.')
   print(f'Seu nome tem {len(nome)} letras.')
   print(f'A primeira letra do seu nome é {nome[0]}.')
   print(f'A última letra do seu nome é {nome[-1]}.')
